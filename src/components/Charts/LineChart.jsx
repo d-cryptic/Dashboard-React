f9d750b9b8c9e673d3ba56babaa4667df162caf6
Barun Debnath <barundebnath91@gmail.com>
2022-06-04 16:41:45 +0530

    Area | Line Chart done

--- src/components/Charts/LineChart.jsx
+++ src/components/Charts/LineChart.jsx
@@ -4,6 +4,7 @@ import {
   SeriesCollectionDirective,
   SeriesDirective,
   Inject,
+  LineSeries,
   DateTime,
   Legend,
   Tooltip,
@@ -14,9 +15,27 @@ import {
   LinePrimaryYAxis,
   LinePrimaryXAxis,
 } from "../../data/dummy";
+import { useStateContext } from "../../contexts/ContextProvider";
 
 const LineChart = () => {
-  return <div>LineChart</div>;
+  const { currentMode } = useStateContext();
+  return (
+    <ChartComponent
+      id="line-chart"
+      height="420px"
+      primaryXAxis={LinePrimaryXAxis}
+      primaryYAxis={LinePrimaryYAxis}
+      chartArea={{ border: { width: 0 } }}
+      tooltip={{ enable: true }}
+      background={currentMode === "Dark" ? "#33373E" : "#FFF"}>
+      <Inject services={[LineSeries, DateTime, Legend, Tooltip]} />
+      <SeriesCollectionDirective>
+        {lineCustomSeries.map((item, index) => (
+          <SeriesDirective key={index} {...item} />
+        ))}
+      </SeriesCollectionDirective>
+    </ChartComponent>
+  );
 };
 
 export default LineChart;
